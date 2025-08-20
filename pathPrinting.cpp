61861e4b6439a1d8f4a734e1bc5a41258728ab7d
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-08-20 16:11:44 +0600

    Explored path printing

--- pathPrinting.cpp
+++ pathPrinting.cpp
@@ -15,6 +15,33 @@ using namespace std;
 
 vector<int> adjacencyList[1005];
 int visitedArray[1005];
+int levelArray[1005];
+int parentArray[1005];
+
+void bfs(int sourceNode)
+{
+    queue<int> integerQueue;
+    integerQueue.push(sourceNode);
+    visitedArray[sourceNode] = true;
+    levelArray[sourceNode] = 0;
+
+    while (!integerQueue.empty())
+    {
+        int parentNode = integerQueue.front();
+        integerQueue.pop();
+
+        for (auto childNode : adjacencyList[parentNode])
+        {
+            if (visitedArray[childNode] == false)
+            {
+                integerQueue.push(childNode);
+                visitedArray[childNode] = true;
+                levelArray[childNode] = parentNode + 1;
+                parentArray[childNode] = parentNode;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -28,6 +55,30 @@ int main()
 
         adjacencyList[firstValue].push_back(secondValue);
         adjacencyList[secondValue].push_back(firstValue);
+    }
+
+    memset(visitedArray, false, sizeof(visitedArray));
+    memset(levelArray, -1, sizeof(levelArray));
+    memset(parentArray, -1, sizeof(parentArray));
+
+    int sourceNode, destinationNode;
+    cin >> sourceNode >> destinationNode;
+
+    bfs(sourceNode);
+
+    int pathNode = destinationNode;
+    stack<int> pathStack;
+
+    while (pathNode != -1)
+    {
+        pathStack.push(pathNode);
+        pathNode = parentArray[pathNode];
+    }
+
+    while (!pathStack.empty())
+    {
+        cout << pathStack.top() << " ";
+        pathStack.pop();
     }
 
     return 0;
